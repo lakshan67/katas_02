ea3cb317235785f31a3f8e1d26b6ceb766694224
lakshan <lakshanrandika255@gmail.com>
2024-06-09 00:51:25 +0530

    kata 1 completed

---
+++ Js File/katas_02.js
@@ -0,0 +1,11 @@
+const peoples = [
+    { name: 'malika', age: 25 },
+    { name: 'chalana', age: 30 },
+    { name: 'eran', age: 35 },
+    { name: 'chathurika', age: 35 },
+    ]
+
+const names = peoples.map(person => person.name);
+
+console.log(names);
+
